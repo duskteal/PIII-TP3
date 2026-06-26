3fb8baa57f48f0f9543e9fdd3b2fe5c24910580b
Mariano Goyochea <mariano.g224@gmail.com>
2026-06-25 23:37:13 -0300

    Iniciada pagina de flexbox, cargado el arreglo y problema de carga del DOM en JS en HTML inactivo

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded",()=>{ // Espera a cargar el conteni
     botonNav.addEventListener("click", ()=>{
         volverPaginaPrincipal()
     });
-    console.log(botonNav)
+
     navPrincipal.appendChild(botonNav)
 })
 
@@ -35,18 +35,23 @@ const listadoPaginas = [
 
 const contenedorTarjetas = document.getElementById("contenedorTarjetas")
 
-listadoPaginas.forEach(pagina => {
-    const tarjeta = document.createElement("button")
-    tarjeta.classList.add("tarjeta")
-    tarjeta.innerHTML = `
-    <h1>${pagina.nombre}</h1>
-    <p class="descripcion">${pagina.descripcion}</p>`
-    contenedorTarjetas.appendChild(tarjeta)
-    tarjeta.addEventListener("click",()=>{
-        window.location.href = `${pagina.link}`
-    })
+if(contenedorTarjetas){
+    listadoPaginas.forEach(pagina => {
+        const tarjeta = document.createElement("button")
+        tarjeta.classList.add("tarjeta")
+        tarjeta.innerHTML = `
+        <h1>${pagina.nombre}</h1>
+        <p class="descripcion">${pagina.descripcion}</p>`
+        contenedorTarjetas.appendChild(tarjeta)
+        tarjeta.addEventListener("click",()=>{
+            window.location.href = `${pagina.link}`
+        }
+        )
     }
-);
+    );
+}
+
+
 const tarjetas = document.querySelectorAll(".tarjeta")
 
 tarjetas.forEach(tarjeta =>{
@@ -58,8 +63,6 @@ tarjetas.forEach(tarjeta =>{
     }
 )
 
-const tarjetaHover = document.querySelectorAll(".tarjetaHover")
-
 tarjetas.forEach(tarjeta =>{
         tarjeta.addEventListener("mouseleave",() =>
             {
@@ -68,3 +71,78 @@ tarjetas.forEach(tarjeta =>{
         )
     }
 )
+
+const motocicletas = [
+    {
+        marca: "Suzuki",
+        modelo: "GSX-R",
+        cilindrada: 1000,
+        motor: "Tetracilíndrico en línea",
+        potencia: 202,
+        estilo: "Superbike",
+        paisOrigen: "Japón"
+    },
+    {
+        marca: "Triumph",
+        modelo: "765 Street Triple",
+        cilindrada: 765,
+        motor: "Tricilíndrico en línea",
+        potencia: 120,
+        estilo: "Naked",
+        paisOrigen: "Reino Unido"
+    },
+    {
+        marca: "Ducati",
+        modelo: "Diavel",
+        cilindrada: 1200,
+        motor: "Bicilindrico en L",
+        potencia: 162,
+        estilo: "Muscle Cruiser",
+        paisOrigen: "Italia"
+    },
+    {
+        marca: "BMW Motorrad",
+        modelo: "R 1250 GS",
+        cilindrada: 1250,
+        motor: "Bicilíndrico tipo Boxer",
+        potencia: 136,
+        estilo: "Adventure",
+        paisOrigen: "Alemania"
+    },
+    {
+        marca: "Kawasaki",
+        modelo: "ZX-10R",
+        cilindrada: 1000,
+        motor: "Tetracilíndrico en línea",
+        potencia: 203,
+        estilo: "Superbike",
+        paisOrigen: "Japón"
+    },
+    {
+        marca: "Honda",
+        modelo: "CRF450RX",
+        cilindrada: 450,
+        motor: "Monocilíndrico Unicam",
+        potencia: 202,
+        estilo: "Enduro",
+        paisOrigen: "Japón"
+    }
+];
+
+const contenedorMotos = document.getElementById("contenedorMotos")
+
+if(contenedorMotos){
+        motocicletas.forEach(moto =>{
+            const tarjetaMoto = document.createElement("div")
+            tarjetaMoto.classList.add("tarjetaMoto")
+            tarjetaMoto.innerHTML = `
+                <h2>${moto.marca} ${moto.modelo}</h2>
+                <p>Motor: ${moto.motor}</p>
+                <p>Cilindrada: ${moto.cilindrada}cc</p>
+                <p>Potencia: ${moto.potencia}CV</p>
+                <p>Estilo: ${moto.estilo}</p>
+                <p>País de orígen: ${moto.paisOrigen}</p>`
+                contenedorMotos.appendChild(tarjetaMoto)
+        }
+        )
+}
